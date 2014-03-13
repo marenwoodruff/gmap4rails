26b462b5a8e92382fa6a5695e81f72f29ed32015
MarenWoodruff <wren403@gmail.com>
2014-03-12 20:58:59 -0700

    finished app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gmaps::Application.config.secret_key_base = '3fe5e66a7e4da2a035c5627cae993c3618a778a589a133b37d7c8f4552e57c7723f0315e011eb8c238a2f18a32c3a495e39d6fc17f7e71e2e0134f01e5ae39d1'
